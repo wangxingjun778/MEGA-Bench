cf92861b6b3c2cb367d6398709f17cb580ec0b15
xingjun.wxj <xingjun.wxj@alibaba-inc.com>
2025-02-23 10:36:13 +0800

    temp

--- megabench/tools/derive_breakdown_results.py
+++ megabench/tools/derive_breakdown_results.py
@@ -45,6 +45,9 @@ def calculate_model_summary(task_results_with_meta):
     open_stats = calculate_stats(open_tasks)
     
     # Calculate overall score (weighted average based on number of tasks)
+    # ex core_stats: {'num_eval_tasks': 440, 'num_eval_samples': 6531, 'macro_mean_score': 0.2466618622594841}
+    print(f'>>core_stats: {core_stats}')
+    print(f'>>open_stats: {open_stats}')
     total_tasks = (core_stats["num_eval_tasks"] + open_stats["num_eval_tasks"])
     overall_score = (
         (core_stats["macro_mean_score"] * core_stats["num_eval_tasks"] + 
